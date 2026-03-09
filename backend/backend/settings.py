42b4bccdc09b71e9857f33ffdfc601c0a16fb5e3
mohammednajeer <mohammednajeer.n@gmail.com>
2026-03-09 12:47:57 +0530

    fix allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["zoyr-ecommerce.onrender.com",
+    "localhost",
+    "127.0.0.1"]
 
 
 # Application definition
