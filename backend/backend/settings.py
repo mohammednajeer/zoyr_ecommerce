1b14d828f4090578558bfe68e41f7c47751fb32c
mohammednajeer <mohammednajeer.n@gmail.com>
2026-03-09 13:01:33 +0530

    connect frontend to production backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -63,7 +63,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://zoyrcars.vercel.app/",
+]
 
 ROOT_URLCONF = 'backend.urls'
 
